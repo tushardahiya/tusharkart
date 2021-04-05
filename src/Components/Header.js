5b115dfcc2a8d4d40d67c696ec6da00f88b509e2
tushardahiya <tushardahiya84@gmail.com>
2021-04-05 17:17:24 +0530

    navbar done with css

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,43 @@
 import React from "react";
+import "./Header.css";
+
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingCartOutlinedIcon from "@material-ui/icons/ShoppingCartOutlined";
 
 function Header() {
-  return <div className="header">Header</div>;
+  return (
+    <div className="header">
+      <img
+        className="header-logo"
+        alt="loading"
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+      />
+
+      <div className="header-search">
+        <input className="header-searchInput" type="text" />
+        <SearchIcon className="header-searchIcon" />
+      </div>
+
+      <div className="header-navs">
+        <div className="header-option">
+          <span className="header-option-line1">Hello Guest</span>
+          <span className="header-option-line2">Sign In</span>
+        </div>
+        <div className="header-option">
+          <span className="header-option-line1">Returns</span>
+          <span className="header-option-line2">& Orders</span>
+        </div>
+        <div className="header-option">
+          <span className="header-option-line1">Your</span>
+          <span className="header-option-line2">Prime</span>
+        </div>
+        <div className="header-basket">
+          <ShoppingCartOutlinedIcon />
+          <span className="header-bascket-count">0</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
