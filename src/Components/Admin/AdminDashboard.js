2a7d7ff254549658696052a04a8dda15e0b5395a
tushardahiya <tushardahiya84@gmail.com>
2021-04-10 22:47:19 +0530

    added component for adding new products

--- src/Components/Admin/AdminDashboard.js
+++ src/Components/Admin/AdminDashboard.js
@@ -1,9 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./AdminDashboard.css";
 
 const AdminDashboard = () => {
+  const [id, setId] = useState("");
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const [rating, setRating] = useState("");
+  const [image, setImage] = useState("");
+  const [product, setProduct] = useState({});
+
+  const addProductHandler = (e) => {
+    e.preventDefault();
+    setProduct({
+      id: id,
+      title: title,
+      price: price,
+      rating: rating,
+      image: image,
+      product: product,
+    });
+  };
+
   return (
-    <div>
-      <h1>ADMIN DASHBOARD</h1>
+    <div className="dashboard">
+      <Link to="/">
+        <img
+          className="adminLogin-logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+          alt=""
+        />
+      </Link>
+      <div className="dashboard-container">
+        <h1>Add Product</h1>
+        <form>
+          <h5>id</h5>
+          <input
+            type="text"
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+          />
+          <h5>title</h5>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <h5>price</h5>
+          <input
+            type="text"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+          />
+          <h5>rating</h5>
+          <input
+            type="text"
+            value={rating}
+            onChange={(e) => setRating(e.target.value)}
+          />
+          <h5>image</h5>
+          <input
+            type="text"
+            value={image}
+            onChange={(e) => setImage(e.target.value)}
+          />
+          <button onClick={addProductHandler} className="dashboard-button">
+            Add product to my catalog
+          </button>
+        </form>
+        <p>
+          Provide all details to add a new product of your choice to your
+          products
+        </p>
+      </div>
     </div>
   );
 };
